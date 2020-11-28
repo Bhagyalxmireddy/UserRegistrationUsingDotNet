740e759eb2c840beba151ac791d7d6a39298eaf3
Bhagi <puppyreddy1997@gmail.com>
2020-11-28 15:53:58 +0530

    [Bhagyalaxmi]ADDED:Added Code and TestCases to validate the firstName

---
+++ UserRegistration/UserValidations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class UserValidations
+    {
+        private const string NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public bool firstName_Validation(String firstName)
+        {
+            if (Regex.IsMatch(firstName, NAME_PATTERN))
+                return true;
+            return false;
+        }
+    }
+}
