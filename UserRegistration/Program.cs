54f66279c45ab33c7a8a2d6a16042f44603e5397
Bhagi <puppyreddy1997@gmail.com>
2020-11-28 15:36:32 +0530

    [Bhagyalaxmi]ADDED:Added Code to display the welcome Message

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To UserRegistration Program");
+        }
+    }
+}
