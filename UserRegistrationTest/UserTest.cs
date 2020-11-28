11f452ac8fae344ee63dd83b92b2e7a2c5ac693b
Bhagi <puppyreddy1997@gmail.com>
2020-11-28 18:28:01 +0530

    [Bhagyalaxmi]ADDED:Added Code and Parameter TestCases to validate the emails

--- UserRegistrationTest/UserTest.cs
+++ UserRegistrationTest/UserTest.cs
@@ -170,6 +170,39 @@ namespace UserRegistrationTest
             bool result = validations.passWord_Validation("Bhagyalaxmi@123");
             Assert.IsTrue(result);
         }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void givenEmails_WithMultipleInputs_ShouldReturnTrue(string email)
+        {
+            bool result = validations.email_Validation(email);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        [DataRow("abc.com")]
+        [DataRow("abc@.com.my")]
+        [DataRow("abc123@gmail.a")]
+        [DataRow("abc123@.com")]
+        [DataRow("abc123@.com.com")]
+        [DataRow(".abc@abc.com")]
+        [DataRow("abc@%*.com")]
+        [DataRow("abc()*@gmail.com")]
+        [DataRow("abc..2002@gmail.com")]
+        [DataRow("abc.@gmail.com")]
+        [DataRow("abc@gmail.com.1a")]
+        [DataRow("abc@gmail.com.aa.au")]
+        public void givenEmails_WithMultipleInputs_WithoutProper_ShouldReturnFalse(string email)
+        {
+            bool result = validations.email_Validation(email);
+            Assert.IsFalse(result);
+        }
     }
 }
 
